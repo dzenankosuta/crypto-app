0b3659b35e40e8ad788d36985bd654f6277f0a2b
dzenankosuta <dzenan_kosuta@hotmail.com>
2023-02-28 19:55:58 +0100

    comments removed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,6 @@
-// import axios from "axios";
 import React, { useContext } from "react";
 import { TableOfCurrencies } from "../components/Tables/TableOfCurrencies";
 import { ApplicationContext } from "../context/ApplicationContext";
-
-// const CORS_PROXY_URL = "https://cors-anywhere.herokuapp.com/";
-// const API_URL = "https://api.bitfinex.com/v1/symbols";
 
 export default function HomePage() {
   const { dataCurrencies } = useContext(ApplicationContext);
